ce873842cf9be18fe1d92cc5aa6c39662a010fba
Warren Morris <warrenmorris@Warrens-MacBook-Pro.local>
2013-02-14 11:40:15 -0800

    added variables

--- sdi-project2/Morris_Warren_assignment2.js
+++ sdi-project2/Morris_Warren_assignment2.js
@@ -1,1 +1,20 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+/*
+// Warren Morris
+// SDI 1302
+// Project 2 
+*/
+
+var sayIt = function (logWhat) { console.log( logWhat ); };
+
+var guitarModel = "Ibanez",
+	stringNumbers = [1,2,3,4,5,6],
+	standardTuning = ["E","A","D","G","B","e"],
+	dTuning = ["D","A","D","G","B","e"]
+	isTuned = false,
+	untunedCount = stringNumbers.length	
+;
+
+
+//sayIt("test")
