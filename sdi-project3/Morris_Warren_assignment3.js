165a8f63a25a1fe7056861e1f204d6fcef32deeb
Warren Morris <robbymorris@fullsail.edu>
2013-02-21 01:09:30 -0800

    saved

--- sdi-project3/Morris_Warren_assignment3.js
+++ sdi-project3/Morris_Warren_assignment3.js
@@ -1,1 +1,52 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Warren Morris
+// SDI 1302
+// Project 3
+// More, not so fun stuff...
+
+//True Random is an illusion, but this should work for this purpose
+var rndPick = Math.floor(Math.random()*10)
+var whatHappened = function (num) {
+
+if (num === 0){
+	console.log(num);
+} else if (num === 1){
+	console.log(num);
+} else if (num === 2){
+	console.log(num);
+} else if (num === 3){
+	console.log(num);
+} else if (num === 4){
+	console.log(num);
+} else if (num === 5){
+	console.log(num);
+} else if (num === 6){
+	console.log(num);
+} else if (num === 7){
+	console.log(num);
+} else if (num === 8){
+	console.log(num);
+} else if (num === 9){
+	console.log(num);
+} else {
+	console.log("Else");
+	}
+};
+
+
+var startStory = function () {
+
+console.log("This is the story about Bob and his life in his eyes.")
+console.log("Bob wakes up every day, wishing he hadn't woken up at all.")
+console.log("It didn't always used to be like that. In fact, Bob used to be very content with his life.")
+console.log("")
+console.log(" ")
+
+};
+
+
+
+
+//console.log(rndPick);
+whatHappened(rndPick);
