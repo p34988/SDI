30343151228e9d972227c2f4366509dfb3e4aef7
Warren Morris <robbymorris@fullsail.edu>
2013-02-21 03:03:24 -0800

    saved

--- sdi-project3/Morris_Warren_assignment3.js
+++ sdi-project3/Morris_Warren_assignment3.js
@@ -36,19 +36,36 @@ var hasStarted = function (bool) {
 
 };
 var startStory = function () {
-	console.log("I have spent more time on this project than i spent at work in a week.");
+	console.log("I have spent more time on this project than I spent at work in a week.");
 	console.log("In an effort to show that I'm able to learn what is going on here, this is going to just be the requirements to the best of my ability, minus the story.");
 	startBool = hasStarted(startBool);
 	console.log("Has started: " + startBool)
 };
 
+var loops = function () {
+	
+	for (var i = 0; i < 6; i++) {
+
+		for (var k = 0, j=i; k < 3; k++) {
+		console.log("Loop 1 #: " + j + "  Loop 2 #: " + k);
+		};
+
+	};	
 
 
+};
+
+console.log("Has started: " + startBool)
 
 // Calling a function
-console.log("Has started: " + startBool)
 startStory();
 
 //Getting an array item
 console.log("Randomly Chosen Array Item: " + pickArrItem(rndPick));
+
+//returning an array
 console.log (pickArrItem(null));
+
+
+loops();
+
